dfbe22155993ba07a1c9076b2a9d3b370eebe0c1
Alex3336 <alexaamogus@gmail.com>
2025-11-05 00:02:23 +0200

    pagination done

--- src/js/api.js
+++ src/js/api.js
@@ -1,23 +1,30 @@
-import { renderEvents, eventHandle} from "./render";
+import { renderEvents, eventHandle } from './render';
+import { currentPage, renderPagination } from './pagination.js';
 const API_KEY = 'q6wGVb9Aq0qhPo2kRkaUMu7npvf9A2ZE';
 const BASE_URL = 'https://app.ticketmaster.com/discovery/v2';
 
-async function getEvents() {
-    try {
-        //тут мож  countryCode любий, тут найбільше адекватних
-      const url = `${BASE_URL}/events.json?apikey=${API_KEY}&countryCode=DE`;
-      const res = await fetch(url);
-      if (!res.ok) throw new Error(`Request failed: ${res.status}`);
-      const data = await res.json();
-   //шлях такий))
-      if (data && data._embedded && data._embedded.events) {
-        eventHandle(data._embedded.events)
-      }
+export async function getEvents() {
+  try {
+    //тут мож  countryCode любий, тут найбільше адекватних
+    const url = `${BASE_URL}/events.json?apikey=${API_KEY}&countryCode=DE&page=${currentPage}`;
+    const res = await fetch(url);
+    if (!res.ok) throw new Error(`Request failed: ${res.status}`);
+    const data = await res.json();
+    //шлях такий))
+    if (data?._embedded?.events) {
+      eventHandle(data._embedded.events);
       renderEvents();
-    } catch (error) {
-      console.error("error get" ,error.message);
     }
+    //всі сторінки як не дивно
+    if (data?.page?.totalPages && data.page.totalPages <= 50) {
+      renderPagination(data.page.totalPages);
+    } else {
+      renderPagination(50);
+    }
+    renderEvents();
+  } catch (error) {
+    console.error('error get', error.message);
   }
-  
-  getEvents();
-  
+}
+
+getEvents();

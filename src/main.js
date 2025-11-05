682bd3e008f079a99aea16447e7101d86b4f9e35
romashka <kazka826@gmail.com>
2025-11-06 00:12:22 +0200

    search and hero done

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
 import './js/api';
-import { getEvents } from './js/api';
 import './js/render';
 import './js/filter';
 import './js/find';
 import './js/modal';
 import './js/pagination';
 
-getEvents();
